0e4343c5a42fcaa3d41949e3c8efa4f39e260dab
liyufeng1 <liyufeng1@pin-dao.cn>
2024-06-03 14:27:56 +0800

    补充

--- src/util/asset.ts
+++ src/util/asset.ts
@@ -1,4 +1,4 @@
-const ASSETS_PATH = 'https://raw.githubusercontent.com/baozouai/multi-platform-posts-action/main/assets/'
+const ASSETS_PATH = 'https://raw.githubusercontent.com/liyufengrex/multi-platform-posts-action/main/assets/'
 
 export function getAssetUrl(asset: string) {
   return ASSETS_PATH + asset
